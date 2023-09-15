3fa66b97d09901a2ef12ccf37d4e68341ae3ab3f
Guanyang Xue <playlego@gmail.com>
2023-09-15 09:26:42 -0400

    New Energy Charts
    Fuel type/unit menu selection and automated conversion to MMBtu.

--- Utility.py
+++ Utility.py
@@ -3,7 +3,7 @@ Extract Data from Energy Charts.xlsx
 Save statistics to Utility.json5
 """
 
-import os, sys, locale, re, openpyxl
+import os, re, openpyxl
 path = os.path.dirname(os.path.abspath(__file__))
 
 # Read Energy Charts.xlsx
@@ -15,15 +15,24 @@ ws = wb['Raw Data']
 EC = round(ws.cell(row=21, column=4).value,3)
 # Get Demand cost from cell D23
 DC = round(ws.cell(row=23, column=4).value,2)
-# Get Natural Gas cost from cell D24
-NGC = round(ws.cell(row=24, column=4).value,2)
+# Get Fuel cost from cell D24
+FC = round(ws.cell(row=24, column=4).value,2)
+# Get Fuel type from cell Q2
+FUEL = ws.cell(row=2, column=17).value
+# Write Natural Gas Cost for compatibility.
+if FUEL == 'Natural Gas':
+    NGC = FC
+else:
+    NGC = 0
 # Open Utility.json5 as text
 with open('utility.json5', 'r') as f:
     utility = f.read()
 # Replace values in Utility.json5
-utility = re.sub(r'EC: \d+\.\d+', 'EC: ' + str(EC), utility)
-utility = re.sub(r'DC: \d+\.\d+', 'DC: ' + str(DC), utility)
-utility = re.sub(r'NGC: \d+\.\d+', 'NGC: ' + str(NGC), utility)
+utility = re.sub(r'EC: .*', 'EC: ' + str(EC) + ',', utility)
+utility = re.sub(r'DC: .*', 'DC: ' + str(DC) + ',', utility)
+utility = re.sub(r'FC: .*', 'FC: ' + str(FC) + ',', utility)
+utility = re.sub(r'FUEL: .*', 'FUEL: "' + FUEL + '",', utility)
+utility = re.sub(r'NGC: .*', 'NGC: ' + str(NGC) + ',', utility)
 # Save utility.json5
 with open('utility.json5', 'w') as f:
     f.write(utility)
