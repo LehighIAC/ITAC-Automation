bbacaa0d6aaf969639678b5944ebc5ee33e5edf1
Guanyang Xue <playlego@gmail.com>
2023-09-23 20:30:23 -0400

    More degree day algorithms
    ez_degree_days uses daily avg. temp and is the same as degreedays.net
    degreee_days uses hourly temp and is usually 10-15% higher than degreedays.net

--- Shared/IAC.py
+++ Shared/IAC.py
@@ -3,15 +3,16 @@
 """
 
 
-def degree_days(ZIP, basetemp, mode):
+def ez_degree_days(ZIP: str, mode: str, temp: int) -> float:
     """
-    Automatically calculate degree days from ZIP code
+    Automatically calculate degree days based on daily average temperature
+    The result should be equal to degreedays.net
     :param ZIP: ZIP code as string
-    :param basetemp: Base temperature as integer
     :param mode: "heating" or "cooling" as string
-    :return: Degree days as integer
+    :param temp: Base temperature as integer
+    :return: Degree days as float
     """
-    from meteostat import Stations, Hourly, units
+    from meteostat import Stations, Daily, units
     from datetime import datetime
     import pgeocode
 
@@ -24,10 +25,10 @@ def degree_days(ZIP, basetemp, mode):
     # 4 years of data, by default
     gap_year = 4
     start = datetime(datetime.now().year - gap_year, 1, 1)
-    end = datetime(datetime.now().year - 1, 12, 31, 23, 59)
+    end = datetime(datetime.now().year - 1, 12, 31)
 
     # Get hourly data
-    data = Hourly(station, start, end)
+    data = Daily(station, start, end)
     data = data.convert(units.imperial)
     data = data.normalize()
     # https://github.com/meteostat/meteostat-python/issues/130
@@ -36,11 +37,72 @@ def degree_days(ZIP, basetemp, mode):
 
     if mode == "cooling":
         # Calculate cooling degree days
-        data['dd'] = data.apply(lambda x: max((x['temp'] - basetemp), 0), axis=1)
+        data['dd'] = data.apply(lambda x: max((x['tavg'] - temp), 0), axis=1)
+        dd = data.dd.sum() / gap_year
+    elif mode == "heating":
+        # Calculate heating degree days
+        data['dd'] = data.apply(lambda x: max((temp - x['tavg']), 0), axis=1)
+        dd = data.dd.sum() / gap_year
+    else:
+        print("Mode must be 'heating' or 'cooling'")
+        exit()
+    return dd
+
+def degree_days(ZIP: str, mode: str, basetemp: int, setback: int=None, hours: list=[9,17], weekend: list=[]) -> float:
+    """
+    Automatically calculate degree days based on hourly data
+    The result is usually higher than degreedays.net
+    :param ZIP: ZIP code as string
+    :param mode: "heating" or "cooling" as string
+    :param basetemp: Base temperature as integer
+    :param setback: Setback temperature as integer, default is None
+    :param hours: Operating hours as list of integer, default is [9, 17] (9am to 5pm)
+    :param weekend: List of weekend schedule as list, default is []. Example: [5,6] (Saturday and Sunday)
+    :return: Degree days as float
+    """
+    from meteostat import Stations, Hourly, units
+    from datetime import datetime
+    import pgeocode
+
+    # Get coordinate from ZIP code
+    location = pgeocode.Nominatim('us').query_postal_code(ZIP)
+
+    # Get closest weather station
+    station = Stations().nearby(location.latitude, location.longitude).fetch(1).index[0]
+
+    # 4 years of data, by default
+    gap_year = 1
+    starttime = datetime(datetime.now().year - gap_year, 1, 1)
+    endtime = datetime(datetime.now().year - 1, 12, 31, 23, 59)
+
+    # Get hourly data
+    data = Hourly(station, starttime, endtime)
+    data = data.convert(units.imperial)
+    data = data.normalize()
+    # https://github.com/meteostat/meteostat-python/issues/130
+    #data = data.interpolate()
+    data = data.fetch()
+
+    # Add a column for set temperature and set it to base temperature
+    data['Tset'] = basetemp
+
+    # If setback temperature is provided
+    if setback != None:
+        # Override time outside weekday hours
+        data['hour'] = data.index.hour
+        data['Tset'] = data.apply(lambda x: setback if (x['hour'] <= hours[0] or x['hour'] >= hours[1]) else x['Tset'], axis=1)
+        # Override time on weekend
+        for holiday in weekend:
+            data['day'] = data.index.weekday
+            data['Tset'] = data.apply(lambda x: setback if (x['day'] == holiday) else x['Tset'], axis=1)
+
+    if mode == "cooling":
+        # Calculate cooling degree days
+        data['dd'] = data.apply(lambda x: max((x['temp'] - x['Tset']), 0), axis=1)
         dd = data.dd.sum() / gap_year / 24
     elif mode == "heating":
         # Calculate heating degree days
-        data['dd'] = data.apply(lambda x: max((basetemp - x['temp']), 0), axis=1)
+        data['dd'] = data.apply(lambda x: max((x['Tset'] - x['temp']), 0), axis=1)
         dd = data.dd.sum() / gap_year / 24
     else:
         print("Mode must be 'heating' or 'cooling'")
@@ -91,7 +153,7 @@ def validate_arc(ARC):
 
     print("")
 
-def grouping_num(dic):
+def grouping_num(dic: dict) -> dict:
     """
     Add thousand separator to numbers in a dictionary and format it to string
     :param dic: EasyDict
@@ -109,12 +171,12 @@ def grouping_num(dic):
             pass
     return dic
 
-def dollar(varlist, dic, digits):
+def dollar(varlist: list, dic: dict, digits: int=0) -> str:
     """
     Format numbers in a dictionary and to currency string
     :param varlist: List of keys in the dictionary
     :param dic: EasyDict
-    :param digits: Number of digits
+    :param digits: Number of digits, default is 0
     :return: Dictionary with keys in formatted currency string
     """
     import locale
@@ -124,7 +186,7 @@ def dollar(varlist, dic, digits):
         dic[var] = locale.currency(dic[var], grouping=True)
     return dic
 
-def combine_words(words):
+def combine_words(words: list) -> str:
     """
     :param words: list of strings
     :return: string of words separated by "," and "and"
@@ -140,7 +202,7 @@ def combine_words(words):
             pass
     return combined
 
-def add_image(doc, tag, image_path, wd):
+def add_image(doc, tag: str, image_path: str, wd):
     """
     Add image to Word document, search for tag in doc and replace with the image
     :param doc: Document
@@ -160,7 +222,7 @@ def add_image(doc, tag, image_path, wd):
     if found_tag == False:
         print("Tag "+ tag +" not found")
 
-def add_eqn(doc, tag, eqn_input):
+def add_eqn(doc, tag: str, eqn_input):
     """
     Add equation to Word document, search for eqn in doc and replace with eqn_input
     :param doc: Document
@@ -195,7 +257,7 @@ def latex2word(latex_input):
     new_dom = transform(tree)
     return new_dom.getroot()
 
-def payback(ACS,IC):
+def payback(ACS, IC) -> str:
     """
     Format payback period by year and month
     :param ACS: Annual Cost Savings ($/yr)
