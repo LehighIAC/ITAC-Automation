054a8e97402a5ae25872697ad40f8e5c49c528bf
Guanyang Xue <playlego@gmail.com>
2023-11-29 15:37:50 -0500

    Fixes automated weather data
    Use python api

--- Compressor/Intake Air/automate.py
+++ Compressor/Intake Air/automate.py
@@ -11,7 +11,7 @@ from Shared.IAC import *
 import numpy as np
 import pgeocode
 from datetime import datetime
-from meteostat import Point, Monthly
+from meteostat import Point, Monthly, units
 
 # Load utility cost
 jsonDict = json5.load(open(os.path.join('..', '..', 'Utility.json5')))
@@ -20,44 +20,40 @@ jsonDict.update(json5.load(open('database.json5')))
 # Convert to easydict
 iac = EasyDict(jsonDict)
 
-## Calculations
-# Compressor Work Reduction
-iac.CWR = int((iac.IT - iac.OT)/(iac.IT + 460)* 10000)
-iac.CWR = float(iac.CWR/100)
-# Operating hours
-iac.OH = iac.HR * iac.DY * iac.WK
-# Power Reduction
-iac.PR = int((iac.HP * 0.746 * iac.LF * iac.CWR)/iac.EHR)
-iac.PR = float(iac.PR/100)
-
 ## Retreiving AVerage Outside Temperature
-# Turn zipcode into a string
-zipcode_num = str(iac.OT)
 # Use US zipcodes
 nomi = pgeocode.Nominatim('us')
-query = nomi.query_postal_code(zipcode_num)
+query = nomi.query_postal_code(iac.ZIP)
 # Return lat and lon information
 lat = query['latitude']
 lon = query['longitude']
-# Start searching from Oct 2018 to May 2022
+
 start = datetime(2018, 10, 1)
-end = datetime(2022, 5, 1)
+end = datetime(2022, 5, 31)
 # Use coordinates for weather info
-place = (lon, lat)
+point = Point(lat, lon)
+# Get monthly data
+data = Monthly(point, start, end)
+data.convert(units.imperial)
+df = data.fetch()
+# Extract average temperature
+df = df['tavg']
+df = df[df.index.month.isin([10,11,12,1,2,3,4,5])]
+iac.TO = round(df.mean())
 
-from urllib.parse import quote
-
-# Assuming place, start, and end are defined elsewhere in your code
-coordinates = f'({lon} {lat})'
-url = f'/v2/monthly/{quote(coordinates)}.csv.gz'
-data = Monthly(place, start, end)
-data = data.fetch(url)
+## Calculations
+# Compressor Work Reduction
+iac.CWR = round((iac.TI - iac.TO)/(iac.TI + 460) * 100, 2)
+# Operating hours
+iac.OH = iac.HR * iac.DY * iac.WK
+# Power Reduction
+iac.PR = round((iac.HP * 0.746 * (iac.FR/100) * (iac.CWR/100)) / (iac.ETA/100), 1)
 
 ## Savings
 # Electrcity Savings
 iac.ES = round(iac.PR * iac.OH)
 # Demand Savings
-iac.DS = round(iac.PR * iac.CF/100 * iac.CC)
+iac.DS = round(iac.PR * (iac.CF/100) * iac.C2)
 # Electrcicity Cost Savings
 iac.ECS = round(iac.ES * iac.EC) 
 # Demand Cost Savings
@@ -70,7 +66,7 @@ iac.PB = payback(iac.ACS, iac.IC)
 
 ## Format strings
 # set to 2 digits accuracy
-iac = dollar(['NGC'],iac,2)
+iac = dollar(['EC','DC'],iac,2)
 # set the rest to integer
 varList = ['ACS', 'ECS', 'DCS', 'IC']
 iac = dollar(varList,iac,0)
